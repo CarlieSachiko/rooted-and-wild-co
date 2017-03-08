e087fe25f7ffa4a936337a33aec3eff4f0778bc3
Carlie Clippinger <carliesachiko@gmail.com>
2017-03-08 10:03:30 -0800

    resposive navbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,5 @@
 $(document).ready (function(){
-
-  if($(window).width() <=768) {
-    $('nav ul.nav li a.nav-link').attr({"data-toggle":"collapse", "data-target": ".navbar-collapse"});
-    $('a#nav-logo-img').remove();
-    $('#logo-div').append('<a ui-sref="home"><img src="images/R&W-logo.png" alt="R&W logo" class="center-block" id="logo-img"></a>');
-  }
-
+  $('.navbar-collapse a').click(function(){
+      $(".navbar-collapse").collapse('hide');
+  });
 });
